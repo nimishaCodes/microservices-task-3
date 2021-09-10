226120dabad5c4e5c29265c90f794156a19ff7ef
nimishaCodes <lnimishakavi@gmail.com>
2021-09-10 16:46:20 +0800

    added more columns to table

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,17 +15,25 @@ function getItems() {
                   <th>name</th>
                   <th>username</th>
                   <th>email</th>
+                    <th>phone</th>
+                <th>website</th>
+                <th>address</th>
+                <th>company</th>
              </tr>`;
 
         // Loop to access all rows 
       //  for (let r of data.list) {
             for (let r of data) {
-            tab += `<tr> 
-            <td>${r.id} </td>
-            <td>${r.name}</td>
-            <td>${r.username}</td> 
-            <td>${r.email}</td>          
-        </tr>`;
+                tab += `<tr> 
+                <td>${r.id} </td>
+                <td>${r.name}</td>
+                <td>${r.username}</td> 
+                <td>${r.email}</td>
+                <td>${r.phone}</td>
+                <td>${r.website}</td>
+                <td>${JSON.stringify(r.address)}</td>
+                <td>${JSON.stringify(r.company)}</td>
+            </tr>`;
         }
         // Setting innerHTML as tab variable
         document.getElementById("employees").innerHTML = tab;
