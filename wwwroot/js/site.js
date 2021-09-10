5e84dc5fef27254729e8679ba8d7f95ffa5b1b95
nimishaCodes <lnimishakavi@gmail.com>
2021-09-10 16:40:58 +0800

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿const uri = 'https://jsonplaceholder.typicode.com/users';
+
+
+function getItems() {
+  fetch(uri)
+    .then(response => response.json())
+    .then(data => show(data))
+    .catch(error => console.error('Unable to get items.', error));
+}
+
+    function show(data) {
+        let tab =
+            `<tr>
+                  <th>id</th>
+                  <th>name</th>
+                  <th>username</th>
+                  <th>email</th>
+             </tr>`;
+
+        // Loop to access all rows 
+      //  for (let r of data.list) {
+            for (let r of data) {
+            tab += `<tr> 
+            <td>${r.id} </td>
+            <td>${r.name}</td>
+            <td>${r.username}</td> 
+            <td>${r.email}</td>          
+        </tr>`;
+        }
+        // Setting innerHTML as tab variable
+        document.getElementById("employees").innerHTML = tab;
+    }
